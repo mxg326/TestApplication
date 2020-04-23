cc1d35847028d0429d0596cdfa8eba03f45adca2
mxg326 <63613858+mxg326@users.noreply.github.com>
2020-04-23 13:36:59 -0400

    Add project files.

---
+++ Controllers/LocationController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DataAccessLayer.Interface;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using BusinessLayer.Interface;
+using Models.Model;
+
+namespace TestApplication.Controllers
+{
+    [ApiController]
+    ////[Authorize]
+    public class LocationController : ControllerBase
+    {
+        private readonly ILocationBusinessLayer locationBusinessLayer;
+        public LocationController(ILocationBusinessLayer locationBusinessLayer)
+        {
+            this.locationBusinessLayer = locationBusinessLayer;
+        }
+
+        [HttpGet]
+        [Route("api/v1/location/")]
+        public async Task<IActionResult> GetAllLocations()
+        {
+            try
+            {
+                var result = await this.locationBusinessLayer.GetAllLocations();
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        [HttpGet]
+        [Route("api/v1/location/{locationId}")]
+        public async Task<IActionResult> GetLocationById(int locationId)
+        {
+            try
+            {
+                var result = await this.locationBusinessLayer.GetLocationsById(locationId);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        [HttpPost]
+        [Route("api/v1/AddLocation/")]
+        public async Task<IActionResult> AddLocation(Location location)
+        {
+            try
+            {
+                var result = await this.locationBusinessLayer.AddLocation(location);
+                return Created("", location);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        [HttpPut]
+        [Route("api/v1/UpdateLocation/")]
+        public async Task<IActionResult> UpdateLocation(Location location)
+        {
+            try
+            {
+                var result = await this.locationBusinessLayer.UpdateLocation(location);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        [HttpDelete]
+        [Route("api/v1/DeleteLocation/{locationId}")]
+        public async Task<IActionResult> DeleteLocation(int locationId)
+        {
+            try
+            {
+                var result = await this.locationBusinessLayer.DeleteLocation(locationId);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BusinessLayer.Interface;
+using BusinessLayer.Utility;
+using DataAccessLayer.Interface;
+using DataAccessLayer.Utility;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace TestApplication
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowOrigin",
+                builder =>
+                {
+                    builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod().AllowCredentials().Build();
+                });
+            });
+            services.AddSingleton<ILocationBusinessLayer, LocationBusinessLayer>();
+            services.AddSingleton<ILocationRepository, LocationRepository>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseCors("AllowOrigin");
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
